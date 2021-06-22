11bbb65f2a6dcecb74bd1a4b5c1dd134fcfc1747
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-06-22 12:55:57 -0700

    Apply linting changes to dialogs

--- src/EPManifest.App/Components/CreateDialog.razor.cs
+++ src/EPManifest.App/Components/CreateDialog.razor.cs
@@ -13,14 +13,15 @@ namespace EPManifest.App.Components
 {
     public partial class CreateDialog : IDisposable
     {
-        [Inject]
-        public IDbContextFactory<EPManifestDbContext> ContextFactory { get; set; }
+        private bool _isLoaded;
 
-        [CascadingParameter]
-        private MudDialogInstance MudDialog { get; set; }
+        private IEntityRepository<Carrier> _carrierRepo;
 
-        [Parameter]
-        public string ContentText { get; set; }
+        private IEntityRepository<Consignee> _consigneeRepo;
+
+        private IEntityRepository<Consignor> _consignorRepo;
+
+        private ICompany _entity;
 
         [Parameter]
         public string ButtonText { get; set; }
@@ -29,15 +30,35 @@ namespace EPManifest.App.Components
         public Color Color { get; set; }
 
         [Parameter]
+        public string ContentText { get; set; }
+
+        [Inject]
+        public IDbContextFactory<EPManifestDbContext> ContextFactory { get; set; }
+
+        [Parameter]
         public ICompany Entity { get; set; }
 
-        private IEntityRepository<Consignor> consignorRepo;
-        private IEntityRepository<Consignee> consigneeRepo;
-        private IEntityRepository<Carrier> carrierRepo;
+        [CascadingParameter]
+        private MudDialogInstance MudDialog { get; set; }
 
-        private ICompany entity;
+        public async Task OnValidSubmit()
+        {
+            switch (Entity)
+            {
+                case Consignor:
+                    await _consignorRepo.Create((Consignor)_entity);
+                    break;
 
-        private bool _isLoaded;
+                case Consignee:
+                    await _consigneeRepo.Create((Consignee)_entity);
+                    break;
+
+                case Carrier:
+                    await _carrierRepo.Create((Carrier)_entity);
+                    break;
+            }
+            Submit();
+        }
 
         protected override async Task OnInitializedAsync()
         {
@@ -46,18 +67,18 @@ namespace EPManifest.App.Components
                 switch (Entity)
                 {
                     case Consignor:
-                        consignorRepo = new ConsignorRepository(ContextFactory.CreateDbContext());
-                        entity = new Consignor();
+                        _consignorRepo = new ConsignorRepository(ContextFactory.CreateDbContext());
+                        _entity = new Consignor();
                         break;
 
                     case Consignee:
-                        consigneeRepo = new ConsigneeRepository(ContextFactory.CreateDbContext());
-                        entity = new Consignee();
+                        _consigneeRepo = new ConsigneeRepository(ContextFactory.CreateDbContext());
+                        _entity = new Consignee();
                         break;
 
                     case Carrier:
-                        carrierRepo = new CarrierRepository(ContextFactory.CreateDbContext());
-                        entity = new Carrier();
+                        _carrierRepo = new CarrierRepository(ContextFactory.CreateDbContext());
+                        _entity = new Carrier();
                         break;
                 }
             }
@@ -69,43 +90,24 @@ namespace EPManifest.App.Components
             await base.OnInitializedAsync();
         }
 
-        private void Submit() => MudDialog.Close(DialogResult.Ok(entity));
-
         private void Cancel() => MudDialog.Cancel();
 
-        public async Task OnValidSubmit()
-        {
-            switch (Entity)
-            {
-                case Consignor:
-                    await consignorRepo.Create((Consignor)entity);
-                    break;
-
-                case Consignee:
-                    await consigneeRepo.Create((Consignee)entity);
-                    break;
-
-                case Carrier:
-                    await carrierRepo.Create((Carrier)entity);
-                    break;
-            }
-            Submit();
-        }
+        private void Submit() => MudDialog.Close(DialogResult.Ok(_entity));
 
         public void Dispose()
         {
             switch (Entity)
             {
                 case Consignor:
-                    consignorRepo.Dispose();
+                    _consignorRepo.Dispose();
                     break;
 
                 case Consignee:
-                    consigneeRepo.Dispose();
+                    _consigneeRepo.Dispose();
                     break;
 
                 case Carrier:
-                    carrierRepo.Dispose();
+                    _carrierRepo.Dispose();
                     break;
             }
         }
--- src/EPManifest.App/Components/EditDialog.razor.cs
+++ src/EPManifest.App/Components/EditDialog.razor.cs
@@ -13,14 +13,15 @@ namespace EPManifest.App.Components
 {
     public partial class EditDialog : IDisposable
     {
-        [Inject]
-        public IDbContextFactory<EPManifestDbContext> ContextFactory { get; set; }
+        private IEntityRepository<Carrier> _carrierRepo;
 
-        [CascadingParameter]
-        private MudDialogInstance MudDialog { get; set; }
+        private IEntityRepository<Consignee> _consigneeRepo;
 
-        [Parameter]
-        public string ContentText { get; set; }
+        private IEntityRepository<Consignor> _consignorRepo;
+
+        private ICompany _entity;
+
+        private bool _isLoaded;
 
         [Parameter]
         public string ButtonText { get; set; }
@@ -29,15 +30,35 @@ namespace EPManifest.App.Components
         public Color Color { get; set; }
 
         [Parameter]
+        public string ContentText { get; set; }
+
+        [Inject]
+        public IDbContextFactory<EPManifestDbContext> ContextFactory { get; set; }
+
+        [Parameter]
         public ICompany Entity { get; set; }
 
-        private IEntityRepository<Consignor> consignorRepo;
-        private IEntityRepository<Consignee> consigneeRepo;
-        private IEntityRepository<Carrier> carrierRepo;
+        [CascadingParameter]
+        private MudDialogInstance MudDialog { get; set; }
 
-        private ICompany entity;
+        public async Task OnValidSubmit()
+        {
+            switch (Entity)
+            {
+                case Consignor:
+                    await _consignorRepo.Update((Consignor)_entity);
+                    break;
 
-        private bool _isLoaded;
+                case Consignee:
+                    await _consigneeRepo.Update((Consignee)_entity);
+                    break;
+
+                case Carrier:
+                    await _carrierRepo.Update((Carrier)_entity);
+                    break;
+            }
+            Submit();
+        }
 
         protected override async Task OnInitializedAsync()
         {
@@ -46,18 +67,18 @@ namespace EPManifest.App.Components
                 switch (Entity)
                 {
                     case Consignor:
-                        consignorRepo = new ConsignorRepository(ContextFactory.CreateDbContext());
-                        entity = consignorRepo.GetById(Entity.Id);
+                        _consignorRepo = new ConsignorRepository(ContextFactory.CreateDbContext());
+                        _entity = _consignorRepo.GetById(Entity.Id);
                         break;
 
                     case Consignee:
-                        consigneeRepo = new ConsigneeRepository(ContextFactory.CreateDbContext());
-                        entity = consigneeRepo.GetById(Entity.Id);
+                        _consigneeRepo = new ConsigneeRepository(ContextFactory.CreateDbContext());
+                        _entity = _consigneeRepo.GetById(Entity.Id);
                         break;
 
                     case Carrier:
-                        carrierRepo = new CarrierRepository(ContextFactory.CreateDbContext());
-                        entity = carrierRepo.GetById(Entity.Id);
+                        _carrierRepo = new CarrierRepository(ContextFactory.CreateDbContext());
+                        _entity = _carrierRepo.GetById(Entity.Id);
                         break;
                 }
             }
@@ -69,43 +90,24 @@ namespace EPManifest.App.Components
             await base.OnInitializedAsync();
         }
 
-        private void Submit() => MudDialog.Close(DialogResult.Ok(entity));
-
         private void Cancel() => MudDialog.Cancel();
 
-        public async Task OnValidSubmit()
-        {
-            switch (Entity)
-            {
-                case Consignor:
-                    await consignorRepo.Update((Consignor)entity);
-                    break;
-
-                case Consignee:
-                    await consigneeRepo.Update((Consignee)entity);
-                    break;
-
-                case Carrier:
-                    await carrierRepo.Update((Carrier)entity);
-                    break;
-            }
-            Submit();
-        }
+        private void Submit() => MudDialog.Close(DialogResult.Ok(_entity));
 
         public void Dispose()
         {
             switch (Entity)
             {
                 case Consignor:
-                    consignorRepo.Dispose();
+                    _consignorRepo.Dispose();
                     break;
 
                 case Consignee:
-                    consigneeRepo.Dispose();
+                    _consigneeRepo.Dispose();
                     break;
 
                 case Carrier:
-                    carrierRepo.Dispose();
+                    _carrierRepo.Dispose();
                     break;
             }
         }
